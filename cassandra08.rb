0fc1adddb230217a9a4486038c8d0d72cb248711
Carlo Cabanilla <carlo@datadoghq.com>
2013-02-06 15:09:49 -0500

    adding cassandra 0.8, our prod version

---
+++ cassandra08.rb
@@ -0,0 +1,72 @@
+require 'formula'
+
+class Cassandra08 < Formula
+  url 'http://www.apache.org/dyn/closer.cgi?path=/cassandra/0.8.10/apache-cassandra-0.8.10-bin.tar.gz'
+  homepage 'http://cassandra.apache.org'
+  md5 '56f6f66940c0af4d0f291fa7071f8cfa'
+
+  def install
+    (var+"lib/cassandra").mkpath
+    (var+"log/cassandra").mkpath
+    (etc+"cassandra").mkpath
+
+    inreplace "conf/cassandra.yaml", "/var/lib/cassandra", "#{var}/lib/cassandra"
+    inreplace "conf/log4j-server.properties", "/var/log/cassandra", "#{var}/log/cassandra"
+
+    inreplace "conf/cassandra-env.sh" do |s|
+      s.gsub! "/lib/", "/"
+    end
+
+    inreplace "bin/cassandra.in.sh" do |s|
+      s.gsub! "CASSANDRA_HOME=`dirname $0`/..", "CASSANDRA_HOME=#{prefix}"
+      # Store configs in etc, outside of keg
+      s.gsub! "CASSANDRA_CONF=$CASSANDRA_HOME/conf", "CASSANDRA_CONF=#{etc}/cassandra"
+      # Jars installed to prefix, no longer in a lib folder
+      s.gsub! "$CASSANDRA_HOME/lib/*.jar", "$CASSANDRA_HOME/*.jar"
+    end
+
+    rm Dir["bin/*.bat"]
+
+    (etc+"cassandra").install Dir["conf/*"]
+    prefix.install Dir["*.txt"] + Dir["{bin,interface,javadoc,lib/licenses}"]
+    prefix.install Dir["lib/*.jar"]
+
+    (prefix+'org.apache.cassandra.plist').write startup_plist
+    (prefix+'org.apache.cassandra.plist').chmod 0644
+  end
+
+  def caveats; <<-EOS.undent
+    If this is your first install, automatically load on login with:
+      mkdir -p ~/Library/LaunchAgents
+      cp #{prefix}/org.apache.cassandra.plist ~/Library/LaunchAgents/
+      launchctl load -w ~/Library/LaunchAgents/org.apache.cassandra.plist
+    EOS
+  end
+
+  def startup_plist; <<-EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>KeepAlive</key>
+    <true/>
+
+    <key>Label</key>
+    <string>org.apache.cassandra</string>
+
+    <key>ProgramArguments</key>
+    <array>
+        <string>#{bin}/cassandra</string>
+        <string>-f</string>
+    </array>
+
+    <key>RunAtLoad</key>
+    <true/>
+
+    <key>WorkingDirectory</key>
+    <string>#{var}/lib/cassandra</string>
+  </dict>
+</plist>
+    EOPLIST
+  end
+end
