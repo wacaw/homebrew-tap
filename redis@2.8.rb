5f16af4a7f42e4a400eb340e198b82a086dec685
Nisanth Chunduru <nisanth074@gmail.com>
2019-07-11 23:38:30 +0530

    Add formula for redis 2.8

---
+++ redis@2.8.rb
@@ -0,0 +1,79 @@
+require 'formula'
+
+class RedisAT28 < Formula
+  homepage 'http://redis.io/'
+  url "http://download.redis.io/releases/redis-2.8.19.tar.gz"
+  version "2.8"
+  sha1 "3e362f4770ac2fdbdce58a5aa951c1967e0facc8"
+
+  bottle do
+    sha1 "ba238ce5e71f5c0c3cb997ebda0cf594f75e8069" => :yosemite
+    sha1 "0902233ed41683e22a1ecd8010f2875c9b0b9dba" => :mavericks
+    sha1 "4b8100b40edd0e6ef695e28bf4fd30360939c3f3" => :mountain_lion
+  end
+
+  head 'https://github.com/antirez/redis.git', :branch => 'unstable'
+
+  fails_with :llvm do
+    build 2334
+    cause 'Fails with "reference out of range from _linenoise"'
+  end
+
+  def install
+    # Architecture isn't detected correctly on 32bit Snow Leopard without help
+    ENV["OBJARCH"] = "-arch #{MacOS.preferred_arch}"
+
+    # Head and stable have different code layouts
+    src = (buildpath/'src/Makefile').exist? ? buildpath/'src' : buildpath
+    system "make", "-C", src, "CC=#{ENV.cc}"
+
+    %w[benchmark cli server check-dump check-aof sentinel].each { |p| bin.install src/"redis-#{p}" }
+    %w[run db/redis log].each { |p| (var+p).mkpath }
+
+    # Fix up default conf file to match our paths
+    inreplace "redis.conf" do |s|
+      s.gsub! "/var/run/redis.pid", "#{var}/run/redis.pid"
+      s.gsub! "dir ./", "dir #{var}/db/redis/"
+      s.gsub! "\# bind 127.0.0.1", "bind 127.0.0.1"
+    end
+
+    etc.install 'redis.conf'
+    etc.install 'sentinel.conf' => 'redis-sentinel.conf'
+  end
+
+  plist_options :manual => "redis-server #{HOMEBREW_PREFIX}/etc/redis.conf"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/redis-server</string>
+          <string>#{etc}/redis.conf</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/redis.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/redis.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/redis-server", "--test-memory", "2"
+  end
+end
